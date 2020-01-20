d8066a011f3d22e1db1cff4d5d2a289707d27e56
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2020-01-20 12:08:38 +0200

    create user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     path: '',
     path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
     controllers: {omniauth_callbacks: 'omniauth_callbacks'}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # get '/users/:id', to: 'users#show'
+  resources :users, only: [:show]
 end
